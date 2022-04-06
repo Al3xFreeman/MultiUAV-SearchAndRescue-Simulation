294b78f5b4d9c239eb3f594793831e09771793f3
Alejandro Díaz Román <alejandrodiazroman.contacto@gmail.com>
2022-04-06 15:12:34 +0200

    increased defualt confidence for image detection file

--- imageDetection.py
+++ imageDetection.py
@@ -10,7 +10,7 @@ ap.add_argument("-i", "--image", required=True,
 #	help="path to Caffe 'deploy' prototxt file")
 #ap.add_argument("-m", "--model", required=True,
 #	help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.2,
+ap.add_argument("-c", "--confidence", type=float, default=0.8,
 	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
